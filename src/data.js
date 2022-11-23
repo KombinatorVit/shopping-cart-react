2fe2fa13c9808f9e90c44f814dfcfde2fb73f0fa
Vit Onischenko <slonik0775@gmail.com>
2022-11-23 22:17:08 +0200

    shopping test cart

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const cart = [
+	{
+		id: 1,
+		img: 'macbook.jpg',
+		title: 'Apple MacBook Air 13',
+		count: 1,
+		price: 30000,
+		priceTotal: 30000,
+	},
+	{
+		id: 2,
+		img: 'apple-watch.jpg',
+		title: 'Apple watch',
+		count: 1,
+		price: 15000,
+		priceTotal: 15000,
+	},
+	{
+		id: 3,
+		img: 'mac-pro.jpg',
+		title: 'Mac Pro',
+		count: 1,
+		price: 30000,
+		priceTotal: 30000,
+	},
+];
+
+export default cart;
